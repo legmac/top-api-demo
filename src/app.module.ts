8516aaf654aceb5ed8aa9545bc7e7681c0aee815
Dmitri Beletki <dmitri.beletki@ussoftwaresolutions.us>
2022-11-06 03:14:04 +0200

    Добавление модулей

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { RevireModule } from './review/review.module';
+import { TopPageModule } from './top-page/top-page.module';
 
 @Module({
-	imports: [],
+	imports: [AuthModule, TopPageModule, ProductModule, RevireModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
