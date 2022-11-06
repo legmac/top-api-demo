5d4beeb925fb06fb0eeac2bb00f8bb1254b941a2
Dmitri Beletki <dmitri.beletki@ussoftwaresolutions.us>
2022-11-06 19:59:30 +0200

    add fix 1

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -9,16 +9,7 @@ export const getMongoConfig = async (configService: ConfigService): Promise<Type
 };
 
 const getMongoString = (configService: ConfigService) =>
-	'mongodb://' +
-	configService.get('MONGO_LOGIN') +
-	':' +
-	configService.get('MONGO_PASSWORD') +
-	'@' +
-	configService.get('MONGO_HOST') +
-	':' +
-	configService.get('MONGO_PORT') +
-	'/' +
-	configService.get('MONGO_AUTHDATABASE');
+	configService.get('MONGO_URL');
 
 const getMongoOptions = () => ({
 	useNewUrlParser: true,
