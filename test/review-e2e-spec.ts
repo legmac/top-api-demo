267f3c694eaacb5ea4a64563f824d85df8d62272
Dmitri Beletki <dmitri.beletki@ussoftwaresolutions.us>
2022-11-06 20:48:00 +0200

    Тесты отзывов

--- test/review-e2e-spec.ts
+++ test/review-e2e-spec.ts
@@ -1,40 +1,82 @@
-import { INestApplication } from "@nestjs/common";
-import { Test, TestingModule } from "@nestjs/testing";
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { Types } from "mongoose";
-import { AppModule } from "src/app.module";
-import { CreateReviewDto } from "src/review/dto/create-review.dto";
+import { AppModule } from './../src/app.module';
+import { CreateReviewDto } from '../src/review/dto/create-review.dto';
+import { Types, disconnect } from 'mongoose';
+import { REVIEW_NOT_FOUND } from '../src/review/review.constants';
 
 const productId = new Types.ObjectId().toHexString();
 
 const testDto: CreateReviewDto = {
-    name: 'Test',
-    title: 'Title-test',
-    description: 'Description of title',
-    rating: 5,
-    productId
+	name: 'Тест',
+	title: 'Заголовок',
+	description: 'Описание тестовое',
+	rating: 5,
+	productId
 };
 
 describe('AppController (e2e)', () => {
-    let app: INestApplication;
-    let createId: string;
+	let app: INestApplication;
+	let createdId: string;
 
-    beforeEach(async ()=> {
-        const moduleFixture: TestingModule = await Test.createTestingModule({
-            imports: [AppModule],
-        }).compile();
-    })
+	beforeEach(async () => {
+		const moduleFixture: TestingModule = await Test.createTestingModule({
+			imports: [AppModule],
+		}).compile();
 
-    it('/review/create (POST)',async (done)=>{
-        return request(app.getHttpServer())
-            .post('/review/crete')
-            .send(testDto)
-            .expect(201)
-            .then(({body}: request.Response )=>{
-                createId = body._id;
-                expect(createId).toBeDefined();
-                done();
-            });
+		app = moduleFixture.createNestApplication();
+		await app.init();
+	});
 
-    });
+	it('/review/create (POST) - success', async (done) => {
+		return request(app.getHttpServer())
+			.post('/review/create')
+			.send(testDto)
+			.expect(201)
+			.then(({ body }: request.Response) => {
+				createdId = body._id;
+				expect(createdId).toBeDefined();
+				done();
+			});
+	});
+
+	it('/review/byProduct/:productId (GET) - success', async (done) => {
+		return request(app.getHttpServer())
+			.get('/review/byProduct/' + productId)
+			.expect(200)
+			.then(({ body }: request.Response) => {
+				expect(body.length).toBe(1);
+				done();
+			});
+	});
+
+	it('/review/byProduct/:productId (GET) - fail', async (done) => {
+		return request(app.getHttpServer())
+			.get('/review/byProduct/' + new Types.ObjectId().toHexString())
+			.expect(200)
+			.then(({ body }: request.Response) => {
+				expect(body.length).toBe(0);
+				done();
+			});
+	});
+
+	it('/review/:id (DELETE) - success', () => {
+		return request(app.getHttpServer())
+			.delete('/review/' + createdId)
+			.expect(200);
+	});
+
+	it('/review/:id (DELETE) - fail', () => {
+		return request(app.getHttpServer())
+			.delete('/review/' + new Types.ObjectId().toHexString())
+			.expect(404, {
+				statusCode: 404,
+				message: REVIEW_NOT_FOUND
+			});
+	});
+
+	afterAll(() => {
+		disconnect();
+	});
 });
