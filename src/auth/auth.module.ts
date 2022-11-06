47bfdaf0ef2d88f805a9230367246c4168245253
Dmitri Beletki <dmitri.beletki@ussoftwaresolutions.us>
2022-11-06 23:07:29 +0200

    Добавление login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { TypegooseModule } from 'nestjs-typegoose';
+import { getJWTConfig } from 'src/configs/jwt.config';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModel } from './user.model';
@@ -15,7 +18,12 @@ import { UserModel } from './user.model';
 					collection: 'User'
 				}
 			}
-		])
+		]),
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJWTConfig
+		})
 	],
 	providers: [AuthService]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,49 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
 import { ModelType } from "@typegoose/typegoose/lib/types";
-import { genSaltSync, hashSync } from "bcryptjs";
+import { compare, genSaltSync, hashSync } from "bcryptjs";
 import { InjectModel } from "nestjs-typegoose";
+import { USER_NOT_FOUND_ERROR, WRONG_PASSWORD_ERROR } from "./auth.constants";
 import { AuthDto } from "./dto/auth.dto";
 import { UserModel } from "./user.model";
+
 
 @Injectable()
 export class AuthService{
     constructor(
-        @InjectModel(UserModel) private readonly userModel: ModelType<UserModel>
+        @InjectModel(UserModel) private readonly userModel: ModelType<UserModel>,
+        private readonly jwtService: JwtService
     ){}
 
     async createUser(dto: AuthDto){
-        const salt = genSaltSync(10);
+        const salt = await genSaltSync(10);
         const newUser = new this.userModel({
 			email: dto.login,
-			passwordHash: hashSync(dto.password, salt)
+			passwordHash: await hashSync(dto.password, salt)
         });
         return newUser.save();
     }
 
-    findUser(email: string){
+    async findUser(email: string){
         return this.userModel.findOne({email}).exec();
+    }
+
+    async validateUser(email: string, password: string): Promise<Pick<UserModel, 'email'>> {
+        const user = await this.findUser(email)
+		if (!user) {
+			throw new UnauthorizedException(USER_NOT_FOUND_ERROR);
+		}
+        const isCorrectPassword = await compare(password, user.passwordHash);
+		if (!isCorrectPassword) {
+			throw new UnauthorizedException(WRONG_PASSWORD_ERROR);
+		}
+        return { email: user.email };
+    }
+
+    async login(email: string) {
+        const payload = { email };
+        return {
+            access_token: await this.jwtService.signAsync(payload)
+        }
     }
 }
