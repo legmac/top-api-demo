69caadebf9684cabe72246103d6e6a9cbe210357
Dmitri Beletki <dmitri.beletki@ussoftwaresolutions.us>
2022-11-06 17:18:43 +0200

    Добавление моделей

--- src/auth/auth.model.ts
+++ src/auth/auth.model.ts
@@ -1,5 +1,10 @@
+import { prop } from "@typegoose/typegoose";
+import { Base, TimeStamps } from "@typegoose/typegoose/lib/defaultClasses";
 
-export class AuthModel {
+export interface AuthModel extends Base { }
+export class AuthModel extends TimeStamps {
+	@prop({ unique: true })
 	email: string;
+	@prop()
 	passwordHash: string;
 }
