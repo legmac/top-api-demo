a7f395793be4dfd98eaa0a3410f38c347fb60c75
Dmitri Beletki <dmitri.beletki@ussoftwaresolutions.us>
2022-11-07 17:30:34 +0200

    Конвератция webp

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -3,6 +3,7 @@ import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 import { FileElementResponse } from './dto/files-elment.respons'
 import { FilesService } from './files.service';
+import { MFile } from './mfile.class';
 
 @Controller('files')
 export class FilesController {
@@ -12,8 +13,16 @@ export class FilesController {
 
 	@Post('upload')
 	@HttpCode(200)
-	@UseInterceptors(FilesInterceptor('files'))
+	@UseInterceptors(FileInterceptor('files'))
 	async uploadFile(@UploadedFile() file: Express.Multer.File): Promise<FileElementResponse[]> {
-		return this.filesService.saveFiles([file]);
+		const saveArray: MFile[] = [new MFile(file)];
+		if (file.mimetype.includes('image')) {
+			const buffer = await this.filesService.convertToWebP(file.buffer);
+			saveArray.push(new MFile({
+				originalname: `${file.originalname.split('.')[0]}.webp`,
+				buffer
+			}));
+		}
+		return this.filesService.saveFiles(saveArray);
 	}
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -3,11 +3,15 @@ import { format } from 'date-fns';
 import { path } from 'app-root-path';
 import { ensureDir, writeFile } from 'fs-extra';
 import { FileElementResponse } from './dto/files-elment.respons';
+import sharp from 'sharp';
+import { MFile } from './mfile.class';
+
+
 
 @Injectable()
 export class FilesService {
 
-	async saveFiles(files: Express.Multer.File[]): Promise<FileElementResponse[]> {
+	async saveFiles(files: MFile[]): Promise<FileElementResponse[]> {
 		const dateFolder = format(new Date(), 'yyyy-MM-dd');
 		const uploadFolder = `${path}/uploads/${dateFolder}`;
 		await ensureDir(uploadFolder);
@@ -17,5 +21,12 @@ export class FilesService {
 			res.push({ url: `${dateFolder}/${file.originalname}`, name: file.originalname });
 		}
 		return res;
+	}
+
+
+	convertToWebP(file: Buffer): Promise<Buffer> {
+		return sharp(file)
+			.webp()
+			.toBuffer();
 	}
 }
